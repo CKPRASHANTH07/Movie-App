658c6d94400ba586c4b80dddb8af76b30a8a778e
CKPRASHANTH07 <103945546+CKPRASHANTH07@users.noreply.github.com>
2024-09-05 15:47:20 +0530

    : )

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,6 @@
-const { PORT, HOST } = require('./utils/config_env.js');
+import { PORT, HOST } from './utils/config_env.js';
+import { app } from './routes/server.js'; // Correct path to import named export
 
 app.listen(PORT, HOST, () =>
   console.log(`Running on port ${PORT} and HOST ${HOST}`)
 );
-
