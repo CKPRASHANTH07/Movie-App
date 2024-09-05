b87b10f7cbb5a801b73c8f3b26e8d2e5b8cf1070
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-09-05 23:13:04 +0530

    modified:   backend/src/main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -5,7 +5,7 @@ import app from '../src/routes/server.js'; // Assuming server.js exports the app
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, '../../../src/dist')));
+app.use(express.static(path.join(__dirname, '../../../dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname,'dist', 'index.html'));
