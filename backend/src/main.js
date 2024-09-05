3ed449dedf3a5e3960a782a0aef310a25d1e151c
CKPRASHANTH07 <103945546+CKPRASHANTH07@users.noreply.github.com>
2024-09-05 15:43:10 +0530

    Update main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,22 +1,6 @@
-import express from 'express'; // Import express
-import path from 'path';
-import { fileURLToPath } from 'url'; // Import fileURLToPath to handle import.meta.url
-import { PORT, HOST } from './utils/config_env.js';
-import { app } from './routes/server.js';
+const { PORT, HOST } = require('./utils/config_env.js');
 
-// Compute the directory name of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Setting up static file serving
-app.use(express.static(path.join(__dirname, '../build'))); // Ensure correct path
-
-// Handling all routes to serve the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html')); // Ensure correct path
-});
-
-// Starting the server
 app.listen(PORT, HOST, () =>
   console.log(`Running on port ${PORT} and HOST ${HOST}`)
 );
+
