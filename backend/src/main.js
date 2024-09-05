6a3e2289b7c3ab6f85adbbfef93b5bfd89432d92
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-09-05 22:28:26 +0530

    modified:   backend/src/main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,20 +1,15 @@
 import express from 'express'; // Import express
 import path from 'path';
-import { fileURLToPath } from 'url'; // Import fileURLToPath to handle import.meta.url
 import { PORT, HOST } from '../src/utils/config_env.js'; // Correct path for config_env.js
 import app from '../src/routes/server.js'; // Assuming server.js exports the app instance
 
-// Compute the directory name of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
-// Setting up static file serving
-app.use(express.static(path.join(__dirname, '/dist'))); // Correct path to frontend build directory
+app.use(express.static(path.join(__dirname, '/dist')));
 
-// Handling all routes to serve the index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist', 'index.html')); // Correct path to index.html
-});
+  res.sendFile(path.join(__dirname,'dist', 'index.html'));
+})
 
 // Starting the server
 app.listen(PORT, HOST, () =>
